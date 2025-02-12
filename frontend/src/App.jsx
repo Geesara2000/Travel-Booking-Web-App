9e11c6cdd6886e3b7e1783eb2f85d85a1c271cab
Geesara2000 <geesaraimal12@gmail.com>
2025-02-12 19:29:05 +0530

    create login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Tours from './pages/Tours';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/tours" element={<Tours/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/contact" element={<Contact/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </div>
     </Router>
